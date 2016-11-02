c44d2ffa5079fe3cefeab8046d5404ac2fed2563
1pavel <pashmik@yandex.ru>
2016-11-02 15:03:08 +0300

    Initial commit

---
+++ src/com/company/Circle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Circle {
+    
+    public double p=3.14;
+    public double D;
+
+    public void Circle() {
+        System.out.println("Длина окружности равна" +  p*D);
+    }
+}
